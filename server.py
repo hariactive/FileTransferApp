9d01c48ca68c311757fc50d60ff2cd4ebacc1ef4
HARI NARAYAN SHUKLA <hari7706shukla@gmail.com>
2024-06-30 19:34:27 +0530

    requirements

--- server.py
+++ server.py
@@ -1,41 +1,34 @@
-from flask import Flask, request, send_from_directory, render_template_string
+from flask import Flask, request
 import os
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
+CHUNKS_FOLDER = 'chunks'
+
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-@app.route('/')
-def index():
-    files = os.listdir(UPLOAD_FOLDER)
-    return render_template_string('''
-    <!doctype html>
-    <title>Upload File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    <h2>Uploaded Files</h2>
-    <ul>
-    {% for file in files %}
-      <li><a href="{{ url_for('uploaded_file', filename=file) }}">{{ file }}</a></li>
-    {% endfor %}
-    </ul>
-    ''', files=files)
+if not os.path.exists(CHUNKS_FOLDER):
+    os.makedirs(CHUNKS_FOLDER)
 
-@app.route('/', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    if file:
-        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-        return 'File uploaded successfully'
-    return 'No file uploaded'
+@app.route('/upload-chunk', methods=['POST'])
+def upload_chunk():
+    chunk = request.files['chunk']
+    chunk_index = int(request.form['chunkIndex'])
+    total_chunks = int(request.form['totalChunks'])
+    file_name = 'uploaded_file.tmp'
 
-@app.route('/files/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+    chunk.save(os.path.join(CHUNKS_FOLDER, f'{file_name}_part_{chunk_index}'))
+
+    if chunk_index == total_chunks - 1:
+        with open(os.path.join(UPLOAD_FOLDER, file_name), 'wb') as f_out:
+            for i in range(total_chunks):
+                with open(os.path.join(CHUNKS_FOLDER, f'{file_name}_part_{i}'), 'rb') as f_in:
+                    f_out.write(f_in.read())
+        for i in range(total_chunks):
+            os.remove(os.path.join(CHUNKS_FOLDER, f'{file_name}_part_{i}'))
+
+    return 'Chunk uploaded successfully'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
